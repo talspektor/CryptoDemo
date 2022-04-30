59336b9ce2bbe6c84dc42985ed9f4422c5ece198
Tal Spektor <tal.s@zemingo.com>
2022-04-30 15:06:34 +0300

    Initial Commit

---
+++ CriptoDemo/CriptoDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CriptoDemoApp.swift
+//  CriptoDemo
+//
+//  Created by Tal talspektor on 30/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct CriptoDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
