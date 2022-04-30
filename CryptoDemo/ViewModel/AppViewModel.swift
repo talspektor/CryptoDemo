2cccefecc29b304fc12eebe12499042b0071b40b
Tal Spektor <tal.s@zemingo.com>
2022-04-30 19:44:49 +0300

    Finish

---
+++ CryptoDemo/ViewModel/AppViewModel.swift
@@ -0,0 +1,43 @@
+//
+//  AppViewModel.swift
+//  CriptoDemo
+//
+//  Created by Tal talspektor on 30/04/2022.
+//
+
+import SwiftUI
+
+class AppViewModel: ObservableObject {
+    @Published var coins: [CryptoModel]?
+    @Published var currentCoin: CryptoModel?
+
+    init(){
+        Task{
+            do{
+                try await fetchCryptoData()
+            }catch{
+                // HANDLE ERROR
+                print(error.localizedDescription)
+            }
+        }
+    }
+
+    // MARK: Fetching Crypto Data
+    func fetchCryptoData()async throws{
+        // MARK: Using Latest Async/Await
+        guard let url = url else{return}
+        let session = URLSession.shared
+
+        let response = try await session.data(from: url)
+        let jsonData = try JSONDecoder().decode([CryptoModel].self, from: response.0)
+
+        // Alternative For DispatchQueue Main
+        await MainActor.run(body: {
+            self.coins = jsonData
+            if let firstCoin = jsonData.first{
+                self.currentCoin = firstCoin
+            }
+        })
+    }
+}
+
